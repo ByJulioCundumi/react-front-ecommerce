395ac2a343a34b9636c1c8d2c035b1f208dc540e
ByJulioCundumi <julio.ce.dev@gmail.com>
2023-08-21 12:18:44 -0500

    products component styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navigation from './components/navigation/Navigation'
+import Products from './components/products/Products'
 
 function App() {
 
   return (
     <>
       <Navigation/>
+      <Products/>
     </>
   )
 }
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { AiFillStar } from 'react-icons/ai'
+import {BsFillBagFill} from "react-icons/bs"
+import "./Products.css"
 
 function Products() {
   return (
-    <div>Products</div>
+    <section className='card-container'>
+        <section className="card">
+            <img className='card-img' src="https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UX575_.jpg" alt="" />
+            <div className="card-details">
+                <h3 className="card-title">Nike Air Vapormax Plus</h3>
+                <section className="card-reviews">
+                    <AiFillStar className='card-stars'/><AiFillStar className='card-stars'/><AiFillStar className='card-stars'/><AiFillStar className='card-stars'/>
+                    <span className="total-reviews">4</span>
+                </section>
+                <section className="card-price">
+                    <div className="price"><del>$500</del> $200</div>
+                </section>
+                <div className="card-bag">
+                    <BsFillBagFill className='bag'/>
+                </div>
+            </div>
+        </section>
+    </section>
   )
 }
 
